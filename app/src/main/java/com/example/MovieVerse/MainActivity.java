98377d36f837c6d539cb6fe0ebff0276d9e168c2
Aprit Singh <apritsingh43@gmail.com>
2024-05-11 18:03:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/MovieVerse/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.MovieVerse;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
