a091153e3ff5a5f34ff267432d88d8f65545ec83
thomas <dixeptor@gmail.com>
2024-05-25 17:58:32 +0200

    merge done

--- app/src/main/java/com/example/MovieVerse/Graphic/FilmAdapter.java
+++ app/src/main/java/com/example/MovieVerse/Graphic/FilmAdapter.java
@@ -10,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.MovieVerse.FilmLogic.Film;
+import com.example.MovieVerse.R;
+
 import java.util.List;
 
 public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.ViewHolder> {
